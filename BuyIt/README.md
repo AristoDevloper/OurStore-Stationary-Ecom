8d3c1dd369d3eb4d0946e4dc24c97cbbbf53e4be
AristoDevloper <aryancodes200@gmail.com>
2026-02-02 23:47:54 +0545

    Delete all files in this branch

--- BuyIt/README.md
+++
@@ -1,2 +0,0 @@
-# Stationary-Site-Project
-A basic ecomerce website for stationary items made using django and django rest framework 
--- BuyIt/StationaryApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class StationaryappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'StationaryApp'
